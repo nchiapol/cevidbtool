65d4ebfc75d4931fea247c2b9ac08f55ecc124b5
Nicola Chiapolini <nicola.chiapolini@physik.uzh.ch>
2024-01-08 20:56:33 +0100

    test settings invalidated

--- instance/config.py
+++ instance/config.py
@@ -2,6 +2,6 @@ SECRET_KEY='dev'
 
 OAUTH = {
         "provider": "cevi.puzzle.ch",
-        "id": "Oqv1Uy4t9FYtyBO-U8Tm3tCeoXREzT0TQmp6pnHT3qg",
-        "secret": "1qj5NnHp3RSbulYVCTX0h3tyymubkcdAnY6UG72EqHY",
+        "id": "replace-with-id",
+        "secret": "replace-with-secret",
         }
